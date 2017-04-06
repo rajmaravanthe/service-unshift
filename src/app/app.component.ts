54f41706fa3f987adc85827972a154247c9f02d3
rajmaravanthe <rraajjpoojary@gmail.com>
2017-04-06 11:57:35 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { UnshiftService } from './unshift.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UnshiftService]
 })
 export class AppComponent {
   title = 'app works!';
